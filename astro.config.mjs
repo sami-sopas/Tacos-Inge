2d64ee9fc8f71c35c4aea7c47f49e8ed62b8d024
sami-sopas <ramirezcuenca@outlook.es>
2023-09-08 22:39:41 -0600

    Config deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   experimental: {
     viewTransitions: true,
   },
